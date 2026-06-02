b6fb94a531d56d717ae9b7efeb73bd17580e0168
agussang <msanguinetti@itba.edu.ar>
2026-06-01 23:33:50 -0300

    fix: changed application.yml. NOW IT BUILDS

--- src/main/java/com/financialapp/notifications/web/controller/config/InternalAuthFilter.java
+++ src/main/java/com/financialapp/notifications/web/controller/config/InternalAuthFilter.java
@@ -15,7 +15,7 @@ import java.io.IOException;
 @Slf4j
 public class InternalAuthFilter extends OncePerRequestFilter {
 
-    @Value("${INTERNAL_AUTH_TOKEN}")
+    @Value("${internal.auth.token}")
     private String internalToken;
 
     @Override
